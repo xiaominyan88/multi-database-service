c2e8edd81bb0fdd1365494a5ecb7be97597067c7
xiaominyan <minyan_xiao@163.com>
2021-08-19 10:13:41 +0800

    补充遗漏的启动类

---
+++ data-clickhouse/src/main/java/data/clickhouse/DataClickHouseApplication.java
@@ -0,0 +1,11 @@
+package data.clickhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataClickHouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataClickHouseApplication.class,args);
+    }
+}
